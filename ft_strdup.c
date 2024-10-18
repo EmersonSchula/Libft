366c2367bc720790016d052170423ef4928037df
Emerson Henrique Schula <eschula@c3r10p3.42sp.org.br>
2024-10-18 18:20:38 -0300

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 14:54:30 by eschula           #+#    #+#             */
+/*   Updated: 2024/10/17 18:18:26 by eschula          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+
+	dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (0);
+	ft_strcpy(dest, src);
+	return (dest);
+}
