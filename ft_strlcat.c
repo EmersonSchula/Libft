328e4389a0067767024379462e85726f517e38d2
Emerson Henrique Schula <eschula@c3r10p3.42sp.org.br>
2024-10-18 18:21:23 -0300

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 11:58:58 by eschula           #+#    #+#             */
+/*   Updated: 2024/10/17 18:12:35 by eschula          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	dest_len = 0;
+	while (dest[dest_len] != '\0' && dest_len < size)
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] != '\0' && (dest_len + i) < (size - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
