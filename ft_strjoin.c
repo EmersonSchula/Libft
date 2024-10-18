1039a6c7d3263ea02552f037306a98a3096b45a1
Emerson Henrique Schula <eschula@c3r10p3.42sp.org.br>
2024-10-18 18:21:02 -0300

    ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 15:21:11 by eschula           #+#    #+#             */
+/*   Updated: 2024/10/17 18:19:03 by eschula          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*joined_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined_str = (char *)calloc(len1 + len2 + 1, sizeof(char));
+	if (!joined_str)
+		return (NULL);
+	ft_memcpy(joined_str, s1, len1);
+	ft_memcpy(joined_str + len1, s2, len2);
+	return (joined_str);
+}
