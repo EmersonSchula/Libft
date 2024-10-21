3811795ef6bf506b41acf10dd7070fe9b259f113
Emerson Henrique Schula <eschula@c2r7p1.42sp.org.br>
2024-10-21 18:06:37 -0300

    feat: add function split in file tests

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,73 @@
 /*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 17:29:55 by eschula           #+#    #+#             */
-/*   Updated: 2024/10/18 17:33:51 by eschula          ###   ########.fr       */
+/*   Updated: 2024/10/21 17:41:37 by eschula          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	word_count(const char *s, char c)
+{
+	size_t	count;
+	int		in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	*word_dup(const char *s, size_t start, size_t end)
+{
+	char	*word;
+	size_t	i;
+
+	word = (char *)calloc((end - start +1), sizeof(char));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		word[i++] = s[start++];
+	word[i] = '\0';
+	return (word);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
-	
+	char	**result;
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	if (!s)
+		return (NULL);
+	result = ft_calloc(word_count(s, c) + 1, sizeof(char *));
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			result[j++] = word_dup(s, start, i);
+		}
+		else
+			i++;
+	}
+	result[j] = NULL;
+	return (result);
 }
---
+++ tests/test_ft_split.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "../libft.h"  // Supondo que ft_split esteja declarada em libft.h
+
+int main(void) {
+    char **result;
+    char *s = "Hello, how are you today?";
+    char delimiter = ' ';
+
+    result = ft_split(s, delimiter);
+
+    if (result) {
+        size_t i = 0;
+        while (result[i]) {
+            printf("result[%zu]: \"%s\"\n", i, result[i]);
+            free(result[i]);  // Libera cada substring
+            i++;
+        }
+        free(result);  // Libera o array de ponteiros
+    }
+
+    return 0;
+}
